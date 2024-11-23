d087b7e0752af618d271b46b08e3b6833f7ab0e7
dwincahya <dwincahya8@gmail.com>
2024-11-24 04:30:31 +0700

    Profile

--- src/assets/components/Hero/index.jsx
+++ src/assets/components/Hero/index.jsx
@@ -4,7 +4,13 @@ import Profile from "./Profile";
 const Hero = () => {
   return (
     <div className="mt-20 ml-24 flex flex-col-reverse lg:flex-row items-center justify-between px-8 py-16 bg-white space-y-8 lg:space-y-0 lg:space-x-8">
-      <Card />
+      <Card
+        Header="Hi, I'm Cahya."
+        Description1="I have a deep passion for technology and a constant drive to learn new things."
+        Description2="I enjoy the process of building something from scratch, from conceptualizing an idea to implementing it into a tangible product."
+        Description3="I believe that technology can be used to make the world a better place."
+        buttonName="Check out my CV"
+      />
       <Profile />
     </div>
   );
