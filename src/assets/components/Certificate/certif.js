ffa8f5ee245828416aa42ef210262b5f02a98504
dwincahya <dwincahya8@gmail.com>
2024-11-25 16:02:39 +0700

    Profile

--- src/assets/components/Certificate/certif.js
+++ src/assets/components/Certificate/certif.js
@@ -3,25 +3,19 @@ const certificates = [
       title: "React Developer",
       description: "Certificate for completing React course.",
       image: "https://via.placeholder.com/300", 
-      file: "/certificates/react-developer.pdf", 
+      file: "/certificates/Amikom-WebDesign.pdf", 
     },
     {
       title: "JavaScript Essentials",
       description: "Certificate for mastering JavaScript basics.",
       image: "https://via.placeholder.com/300",
-      file: "/certificates/javascript-essentials.pdf",
+      file: "/certificates/Amikom-HMIF.pdf",
     },
     {
       title: "Web Design Basics",
       description: "Certificate for learning web design principles.",
       image: "https://via.placeholder.com/300",
-      file: "/certificates/web-design-basics.pdf",
-    },
-    {
-      title: "Full Stack Development",
-      description: "Certificate for Full Stack Web Development.",
-      image: "https://via.placeholder.com/300",
-      file: "/certificates/full-stack-development.pdf",
+      file: "/certificates/Amikom-UXDesign.pdf",
     },
   ];
   
