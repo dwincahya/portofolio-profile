fb2c4997ad5b3c495f8cb643264c55ed23dedc02
dwincahya <dwincahya8@gmail.com>
2024-11-28 19:32:22 +0700

    Profile

--- src/assets/components/Footer/index.jsx
+++ src/assets/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="mt-auto text-center text-white bg-neutral-600 w-full">
-      <div className=" p-4 text-center bg-neutral-700 text-neutral-200">
+    <footer className="mt-auto text-center text-white bg-neutral-700 w-full">
+      <div className=" p-4 text-center bg-neutral-800 text-neutral-200">
         © 2024 Copyright:{" "}
         <a className="text-neutral-400" href="/">
           Dwi Nur Cahya.
