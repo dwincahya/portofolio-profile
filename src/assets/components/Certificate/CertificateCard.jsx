d7e201680ae94a9f5332b600626f447ff92c94ce
dwincahya <dwincahya8@gmail.com>
2024-11-28 19:40:15 +0700

    Profile

--- src/assets/components/Certificate/CertificateCard.jsx
+++ src/assets/components/Certificate/CertificateCard.jsx
@@ -2,18 +2,18 @@ import certificates from "./certif";
 
 const CertificateCard = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 pb-10 gap-4 justify-items-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 pb-10 gap-16 justify-items-center">
       {certificates.map((certificate, index) => (
         <div
           key={index}
-          className="bg-white shadow-md rounded-lg overflow-hidden transform transition duration-300 hover:shadow-xl hover:shadow-gray-500 hover:scale-105 cursor-pointer"
-          style={{ width: "372px", height: "292px" }} 
+          className="bg-white shadow-md shadow-gray-400 rounded-lg overflow-hidden transform transition duration-300 hover:shadow-lg hover:shadow-gray-700 hover:scale-105 cursor-pointer"
+          style={{ width: "540px", height: "340px" }} 
           onClick={() => window.open(certificate.file, "_blank")}
         >
           <img
             src={certificate.image}
             alt={certificate.title}
-            className="w-full h-[180px] object-cover"
+            className="w-full h-[252px] object-cover"
           />
           <div className="p-4">
             <h3 className="text-lg font-bold mb-2">{certificate.title}</h3>
