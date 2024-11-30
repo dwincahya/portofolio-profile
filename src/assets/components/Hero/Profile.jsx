d7c738d117d9388c6265e1373a70b371ff74686c
dwincahya <dwincahya8@gmail.com>
2024-11-30 08:46:06 +0700

    Profile

--- src/assets/components/Hero/Profile.jsx
+++ src/assets/components/Hero/Profile.jsx
@@ -3,12 +3,12 @@ import img from "../../img/profile.png";
 const PhotoProfile = () => {
   return (
     <div className="relative flex-shrink-0 flex items-center justify-center">
-      <div className="absolute top-6 right-72 w-60 h-60 bg-gradient-to-br from-blue-600 to-blue-300 rounded-lg z-0"></div>
-      <div className="absolute top-36 right-14 w-60 h-60 bg-gradient-to-br from-blue-300 to-blue-600 rounded-lg z-0"></div>
+      <div className="absolute top-6 right-46 w-60 h-60 bg-gradient-to-br from-blue-600 to-blue-300 rounded-lg z-0"></div>
+      <div className="absolute top-36 left-72 w-60 h-60 bg-gradient-to-br from-blue-300 to-blue-600 rounded-lg z-0"></div>
       <img
         src={img}
         alt="Person"
-        className="relative z-10 right-20 -top-16 w-96 h-auto object-cover"
+        className="relative z-10 left-28 -top-24 w-96 h-auto object-cover"
       />
     </div>
   );
--- src/assets/components/Hero/index.jsx
+++ src/assets/components/Hero/index.jsx
@@ -4,6 +4,7 @@ import PhotoProfile from "./Profile";
 const Hero = () => {
   return (
     <div className="mt-20 md:mt-24 lg:mt-28 xl:mt-32 px-2 sm:px-5 lg:px-20 flex flex-col-reverse lg:flex-row items-center justify-between bg-white space-y-8 lg:space-y-0 lg:space-x-8">
+      <PhotoProfile />
       <Card
         Header="Hi, I'm Cahya."
         Description1="I have a deep passion for technology and a constant drive to learn new things."
@@ -12,7 +13,6 @@ const Hero = () => {
         buttonName="Check out my CV"
         buttonLink="/CV/CV-DwiNurCahya.pdf"
       />
-      <PhotoProfile />
     </div>
   );
 };
