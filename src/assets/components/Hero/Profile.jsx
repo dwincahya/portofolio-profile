def44f71361866e4091d7d15732adb821d04b7d8
dwincahya <dwincahya8@gmail.com>
2024-11-30 08:51:51 +0700

    Profile

--- src/assets/components/Hero/Profile.jsx
+++ src/assets/components/Hero/Profile.jsx
@@ -3,12 +3,12 @@ import img from "../../img/profile.png";
 const PhotoProfile = () => {
   return (
     <div className="relative flex-shrink-0 flex items-center justify-center">
-      <div className="absolute top-6 right-46 w-60 h-60 bg-gradient-to-br from-blue-600 to-blue-300 rounded-lg z-0"></div>
-      <div className="absolute top-36 left-72 w-60 h-60 bg-gradient-to-br from-blue-300 to-blue-600 rounded-lg z-0"></div>
+      <div className="absolute top-6 right-46 w-48 h-48 bg-gradient-to-br from-blue-700 to-blue-400 rounded-lg z-0"></div>
+      <div className="absolute top-36 left-64 w-48 h-48 bg-gradient-to-br from-blue-400 to-blue-700 rounded-lg z-0"></div>
       <img
         src={img}
         alt="Person"
-        className="relative z-10 left-28 -top-24 w-96 h-auto object-cover"
+        className="relative z-10 left-24 -top-24 w-96 h-auto object-cover"
       />
     </div>
   );
