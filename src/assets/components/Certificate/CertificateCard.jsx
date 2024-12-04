41b6a7fdb80bacc0f328cd9fe0141dde9d714578
dwincahya <dwincahya8@gmail.com>
2024-12-04 17:49:55 +0700

    Profile

--- src/assets/components/Certificate/CertificateCard.jsx
+++ src/assets/components/Certificate/CertificateCard.jsx
@@ -1,18 +1,17 @@
 
-const CertificateCard = ({data}) => {
+const CertificateCard = ({ data }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 pb-10 gap-16 justify-items-center">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 pb-10 gap-4 sm:gap-6 md:gap-8 lg:gap-20 justify-items-center">
       {data.map((certificate, index) => (
         <div
           key={index}
-          className="bg-white shadow-md shadow-gray-400 rounded-lg overflow-hidden transform transition duration-300 hover:shadow-lg hover:shadow-gray-700 hover:scale-105 cursor-pointer"
-          style={{ width: "540px", height: "340px" }} 
+          className="bg-white shadow-md shadow-gray-400 rounded-lg overflow-hidden transform transition duration-300 hover:shadow-lg hover:shadow-gray-700 hover:scale-105 pb-3 cursor-pointer w-full sm:w-[300px] md:w-[340px] lg:w-[400px]"
           onClick={() => window.open(certificate.file, "_blank")}
         >
           <img
             src={certificate.image}
             alt={certificate.title}
-            className="w-full h-[252px] object-cover"
+            className="w-full h-48 object-cover"
           />
           <div className="p-4">
             <h3 className="text-lg font-bold mb-2">{certificate.title}</h3>
--- src/assets/components/Certificate/index.jsx
+++ src/assets/components/Certificate/index.jsx
@@ -1,11 +1,10 @@
 import CertificateCard from "./CertificateCard";
-import certificates from "@/assets//Data/certif";
-
+import certificates from "@/assets/Data/certif";
 
 const Certificate = () => {
   return (
-    <div className="items-center mx-24 my-20 justify-center text-center">
-      <div className="text-2xl font-semibold mb-16">CERTIFICATE</div>
+    <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-24 my-10 md:my-20 text-center">
+      <div className="text-xl md:text-2xl font-semibold mb-8 md:mb-16">CERTIFICATE</div>
       <CertificateCard data={certificates} />
     </div>
   );
