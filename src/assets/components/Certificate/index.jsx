96644f41bd0d1e913f6722151ea243b97238ac97
dwincahya <dwincahya8@gmail.com>
2024-12-13 15:10:26 +0700

    Profile

--- src/assets/components/Certificate/index.jsx
+++ src/assets/components/Certificate/index.jsx
@@ -1,5 +1,5 @@
 import CertificateCard from "./CertificateCard";
-import certificates from "@/assets/Data/certif";
+import certificates from "../../Data/Certif";
 
 const Certificate = () => {
   return (
