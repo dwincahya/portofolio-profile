5ef733c3f57893c73f3ef7a7e5deedb47b79bb6a
dwincahya <dwincahya8@gmail.com>
2025-02-01 09:18:35 +0700

    Update Hero

--- src/assets/components/Hero/Profile.jsx
+++ src/assets/components/Hero/Profile.jsx
@@ -2,13 +2,13 @@ import img from "@/assets/img/profile.jpg";
 
 const PhotoProfile = () => {
   return (
-    <div className="flex-shrink-0 flex items-center justify-center my-10 md:my-0 md:mx-8">
+    <div className="flex-shrink-0 flex items-center justify-center my-10 md:mx-8">
       <div className="max-w-96 bg-gradient-to-r from-blue-500 via-blue-700 to-purple-600 rounded-full p-2">
         <div className="rounded-full overflow-hidden flex items-center justify-center">
           <img
             src={img}
             alt="Person"
-            className="w-96 h-96 object-cover"
+            className="md:w-96 h-auto w-56 object-cover"
           />
         </div>
       </div>
